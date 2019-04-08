cbaa58bd3628066601f5e1a507d9acffa78aad3a
galaxy_qing <1945697586@qq.com>
2019-04-08 22:53:38 +0800

    商品销量统计

---
+++ ACE_Behind_Mall.MVC/Controllers/StatisticsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACE_Behind_Mall.MVC.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Statistics
+        public ActionResult GoodSaleList()
+        {
+            return View();
+        }
+    }
+}
