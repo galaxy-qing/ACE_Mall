0343f974de330236aadfacd569ecaa7aed6328f6
galaxy_qing <1945697586@qq.com>
2019-04-20 23:34:36 +0800

    Update Global.asax.cs

--- ACE_Behind_Mall.WebApi/Global.asax.cs
+++ ACE_Behind_Mall.WebApi/Global.asax.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.SessionState;
 
 namespace ACE_Behind_Mall.WebApi
 {
@@ -13,9 +11,16 @@ namespace ACE_Behind_Mall.WebApi
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            //注册定时任务
+            TimeJob.Start();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+        public override void Init()
+        {
+            this.PostAuthenticateRequest += (sender, e) => HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required);
+            base.Init();
         }
     }
 }
