d84041f3601a78b74e8470bbd4713cf04fe0cc2f
galaxy_qing <1945697586@qq.com>
2019-04-20 23:31:41 +0800

    添加票据验证

---
+++ ACE_Behind_Mall.WebApi/App_Start/BasicController.cs
@@ -0,0 +1,44 @@
+﻿using ACE_Mall.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using ACE_Mall.BLL;
+using System.Web.Security;
+
+namespace ACE_Behind_Mall.WebApi.App_Start
+{
+    [AllowAnonymous]
+    public class BasicController : ApiController
+    {
+        //protected int userId = Convert.ToInt32(HttpRuntime.Cache.Get("userid"));
+        protected ModelResponse<dynamic> mr = new ModelResponse<dynamic>();
+        //protected static int userId = GetTicket();
+        UserBLL userbll = new UserBLL();
+        //int aaa=ValidateTicket("111");
+        public int GetTicket()
+        {
+           string ticket = HttpContext.Current.Request.Headers["Authorization"].Substring(10);
+            //解密Ticket
+            var strTicket = FormsAuthentication.Decrypt(ticket).UserData;
+            //从Ticket里面获取用户名和密码
+            var index = strTicket.IndexOf("&");
+            string strUser = strTicket.Substring(0, index);
+            string strPwd = strTicket.Substring(index + 1);
+            var userList = userbll.GetList(x => (x.Account == strUser || x.Email == strUser) & x.Password == strPwd & x.IsDelete == 0);
+            if (userList.Count() > 0)
+            {
+                int userid = userList.FirstOrDefault().ID;
+                //HttpRuntime.Cache.Insert("userid", userid);
+                return userid;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ ACE_Behind_Mall.WebApi/App_Start/RequestAuthorizeAttribute.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+using ACE_Mall.BLL;
+using ACE_Mall.Common;
+
+namespace ACE_Behind_Mall.WebApi.App_Start
+{
+    public class RequestAuthorizeAttribute: AuthorizeAttribute
+    {
+        UserBLL userbll = new UserBLL();
+        //重写基类的验证方式，加入我们自定义的Ticket验证
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            //从http请求的头里面获取身份验证信息，验证是否是请求发起方的ticket
+            var authorization = actionContext.Request.Headers.Authorization;
+            if (authorization != null)
+            {
+                //解密用户ticket,并校验用户名密码是否匹配
+                var encryptTicket = authorization.Parameter;
+                if (ValidateTicket(encryptTicket))
+                {
+                    base.IsAuthorized(actionContext);
+                }
+                else
+                {
+                    HandleUnauthorizedRequest(actionContext);
+                }
+            }
+            //如果取不到身份验证信息，并且不允许匿名访问，则返回未验证401
+            else
+            {
+                var attributes = actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().OfType<AllowAnonymousAttribute>();
+                bool isAnonymous = attributes.Any(a => a is AllowAnonymousAttribute);
+                if (isAnonymous) base.OnAuthorization(actionContext);
+                else HandleUnauthorizedRequest(actionContext);
+            }
+        }
+
+        //校验用户名密码（正式环境中应该是数据库校验）
+        private bool ValidateTicket(string encryptTicket)
+        {
+            //解密Ticket
+            var strTicket = FormsAuthentication.Decrypt(encryptTicket).UserData;
+            //从Ticket里面获取用户名和密码
+            var index = strTicket.IndexOf("&");
+            string strUser = strTicket.Substring(0, index);
+            string strPwd = strTicket.Substring(index + 1);
+            var userList = userbll.GetList(x => (x.Account == strUser || x.Email == strUser) & x.Password == strPwd & x.IsDelete == 0);
+            if (userList.Count()>0)
+            {
+                int userid = userList.FirstOrDefault().ID;
+                //HttpRuntime.Cache.Insert("userid", userid);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
