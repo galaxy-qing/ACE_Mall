7d5ed6efa585c14accad41c9c4ff84dd4b7b7357
galaxy_qing <1945697586@qq.com>
2019-05-12 20:31:35 +0800

    增加类库DistinctHelper

---
+++ ACE_Mall.Common/DistinctHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE_Mall.Common
+{ 
+    public delegate bool CompareDelegate<T>(T x, T y);
+    public class DistinctHelper<T>: IEqualityComparer<T>
+    {
+        private CompareDelegate<T> _compare;
+        public DistinctHelper(CompareDelegate<T> d)
+            {
+                this._compare = d;
+            }
+        public bool Equals(T x, T y)
+            {
+                if (_compare != null)
+                {
+                    return this._compare(x, y);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        public int GetHashCode(T obj)
+            {
+                return obj.ToString().GetHashCode();
+            }
+        
+    }
+}
