3634fdba7d711f007a666627e9d375e1fcc6511c
galaxy_qing <1945697586@qq.com>
2019-05-15 19:38:38 +0800

    支付宝沙箱完善

--- ACE_Behind_Mall.WebApi/Controllers/OrderController.cs
+++ ACE_Behind_Mall.WebApi/Controllers/OrderController.cs
@@ -1,26 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using ACE_Mall.BLL;
 using ACE_Mall.Common;
 using NLog.Fluent;
 using ACE_Mall.Model;
-using System.Web.Http.Description;
 using static ACE_Behind_Mall.WebApi.Models.UserBindingModels;
-using System.Web;
-using System.Net.Http.Headers;
-using System.Collections.Specialized;
-using System.Web.Security;
 using ACE_Behind_Mall.WebApi.App_Start;
 using Aop.Api;
 using Aop.Api.Domain;
 using Aop.Api.Request;
 using Aop.Api.Response;
 using ACE_Mall.Common.App_Code;
-using Newtonsoft.Json;
 
 namespace ACE_Behind_Mall.WebApi.Controllers
 {
@@ -75,21 +67,15 @@ namespace ACE_Behind_Mall.WebApi.Controllers
                     shopcartbll.Update(m);
                 }
                 mr.message = "提交订单成功";
-
                 DefaultAopClient client = new DefaultAopClient(config.gatewayUrl, config.app_id, config.private_key, "json", "1.0", config.sign_type, config.alipay_public_key, config.charset, false);
-
                 // 外部订单号，商户网站订单系统中唯一的订单号
                 string out_trade_no = myorder.OrderNo;
-
                 // 订单名称
-                string subject = "ACE货物购买";
-
+                string subject = "ACE商品购买";
                 // 付款金额
                 string total_amout = model.payMoney.ToString();
-
                 // 商品描述
                 string body = model.note;
-
                 // 组装业务参数model
                 AlipayTradePagePayModel paymodel = new AlipayTradePagePayModel();
                 paymodel.Body = body;
@@ -97,11 +83,12 @@ namespace ACE_Behind_Mall.WebApi.Controllers
                 paymodel.TotalAmount = total_amout;
                 paymodel.OutTradeNo = out_trade_no;
                 paymodel.ProductCode = "FAST_INSTANT_TRADE_PAY";
-
                 AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();
                 // 设置同步回调地址
+                // request.SetReturnUrl("http://192.168.0.198:8088/dist/view/order.html?type=2");
                 request.SetReturnUrl("http://192.168.0.198:8088/dist/view/order.html?type=2");
                 // 设置异步通知接收地址
+                //request.SetNotifyUrl("http://192.168.0.144:60391/Notify_url.aspx");
                 request.SetNotifyUrl("http://192.168.0.144:60391/Notify_url.aspx");
                 // 将业务model载入到request
                 request.SetBizModel(paymodel);
@@ -123,19 +110,6 @@ namespace ACE_Behind_Mall.WebApi.Controllers
                 Log.Error(e.Message);
             }
             return mr;
-        }
-        public static string AliPayInfo(string out_trade_no, string total_amount, string subject)
-        {
-            Dictionary<string, object> dic = new Dictionary<string, object>();
-            //Log.Error("用户个人佣金或余额提现开始", "");
-            dic.Add("out_trade_no", out_trade_no);//商户转账唯一订单号
-            dic.Add("total_amount", total_amount);//收款方账户类型
-            dic.Add("subject", "");//收款方账户
-            //dic.Add("store_id", "");//转账金额
-            //dic.Add("timeout_express", "90m");//付款方姓名
-            //dic.Add("payee_real_name", payee_real_name);//收款方姓名
-            //dic.Add("remark", remark);//转账备注
-            return JsonConvert.SerializeObject(dic);
         }
         /// <summary>
         /// 订单支付
@@ -167,7 +141,6 @@ namespace ACE_Behind_Mall.WebApi.Controllers
         /// <summary>
         /// 获取订单状态下的商品数量
         /// </summary>
-        /// <param name="userId">用户ID</param>
         /// <returns></returns>
         [HttpGet]
         [AllowAnonymous]
@@ -227,7 +200,6 @@ namespace ACE_Behind_Mall.WebApi.Controllers
         /// <summary>
         ///  获取用户订单列表
         /// </summary>
-        /// <param name="userId">用户ID</param>
         /// <param name="orderStatus">订单状态</param>
         /// <returns></returns>
         [HttpGet]
@@ -415,34 +387,6 @@ namespace ACE_Behind_Mall.WebApi.Controllers
                 }
             }
             return mr;
-        }
-        public class config
-        {
-            public config()
-            {
-                //
-                // TODO: 在此处添加构造函数逻辑
-                //
-            }
-            // 应用ID,您的APPID
-            public static string app_id = "2016092700604341";
-
-            // 支付宝网关
-            public static string gatewayUrl = "https://openapi.alipaydev.com/gateway.do";
-
-            // 商户私钥，您的原始格式RSA私钥
-            public static string private_key = "MIIEpAIBAAKCAQEAtaWn9+3hgjIJLisH6W6fXk0PLuzFOffFdTZCfNFzwRMPu0ngXN7bNaXSOP55nPizsDvnjOTQPgGlEQj5q2G/6hfvHcSukLWR4Gp8wTgHzZAa8Us7w+sr49XK2PyursU8vwC3zc2JOLytH9mVGg8ky5b5uZYJpiZAM93NEFn0qKQObDrPuKi6S4roee7DrD4TglU20y2QbDJV9QUO/h2khz40N1ivkb/Ssz2/6gegCO1B+qbDqtfn/yMpqIpGLh3ex5TiRUi0o7bE65aI8cy403CXB088T47d/ivemj719+PMDoz4j4DyrZuytJAcKyalejFy/dTf7xfzE3nBrCubLwIDAQABAoIBAErjR/OcPAucK1LAZNulQsjzBh53ePxkHSEsxI6HH6zq+eJiG8DOFBvzCE99ApBnrFUs7VKWBxB5Rx9VzvEMNL4DLg7cxodchA68eECEpBronWL7fGFSdF7TBnUc/7uID3pDhoOviDI+/zURVDpRIf9ZeA0+QS4huPhpKiDipU5c2lN80VQb4Mx/CPzILHgVlUA9Haw7cVQdaMS4g9EDapvSe840awtqI0623tLzWlVVRFO1kf0OcYaqrZXnNPrUwz7c4wbqP88s//+Vlj4rt5yXq31iIpNOAtnpx8Hf5GLf3oY1ixSSentK2/tI9FCYMseDsfMYvrYls1jtSmazBckCgYEA6FmYe6Efrf0HBPIOA/u9+mgtMBCqohGFqPkXrocz6B0Es1XgyJGpjNR1t41YItFaGBKeBMF28Sq238Ga+TwN3c6JUgS++ltpqsjAassG3h82fIKNnOBdywFFfinWOrI4xIB8JL/dFqdXPTjB8XYHsEnasOq43G5vWHxEh+3ow7sCgYEAyCLhzVA7DWIewm/eAhiSPTjLX39kCbF3Xxv/LzMvUpamsdl6nodxRYyrw6Ipor/zG9/ZUVRYZySnXwjur5GLhragHsFTl1hCMechSuc+n1KJ+bnKWXfYmzoZ6dFUjqN08/XVUDbc5bgpzb1502ks6R7hvWCKXmBkmTOOJMGv3R0CgYEAln7Z7lHZpCd88W4bN+dKETSC+FwDOcBhs6XL+gamz1RkZAhe9WqryIUgzkgl6z9wvKDqaygoc2L4WowbZg3I6X9KoK5kSOMG2VD4mPNyOlM7RoCFWzRUbBYhves5UIF5lYWnY2JGwNKZBhWKQWzy4/OiS+9s15JMWqL6/JRTrNsCgYBRC2cbCFFRgxlnrXtep+qYZiZdHq45CqeHWUQGLhkMvbr1LFduWPSysFtiFon7wPVGpWhQefJumjY40totOKgivlOrAKjEEdaEdM9TkL8YL4l2GhlqD5Ekkuupdr5iIKkcncFrATyEvgYXrZHm3aF1Ka9KTAzcWPaD34/BLLn1tQKBgQCkr4B6z5vwbA2NVSL6uQp3fM2m6EJb5YFPIOnL6Zx/25g9pxdISDD2SK+f4UhCXPJchrr2Cq4lRnOQ7u0Bnw4ZFGdSb0hD/DTuhaTFxNgQQWbOcQbOyIkEQuZTSfMkcoFvpWEcqNAhEN6PSVklu9tAWfwEi/kM3EXc5nhq+4MiwQ==";
-
-
-            // 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
-            public static string alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu3wgGzFzl6gNeCdastRmI5nL/9pno2ENN13quRjpviAYzmG6CnIIIC0JhnlkwR3bU7Yj4FX6az3TD5P8UPp+MSxW8XC9vaPX2zUKxs35ZaBSuRePusdv3rqQ6HbtroLt5jVZ5fI1qqyn9HqoI/oFaOzi+DXmFntlKszMrwKBIbUE90ooHdAmQA2Pv1e1mJRi/4q0svU0mrNigQUgcu/PHsOt9PWmUKyl2EBGCLx2HHGLgKPY8ttfUYqzEFZGW7ZjP45982rwSIZH64WYul86ClVutrOmlZkHo6cDkugGURx5SEqzTvngvMRsPpzvk20OWOSZ7CQuUmeDtqmWz4Y0IQIDAQAB";
-
-
-            // 签名方式
-            public static string sign_type = "RSA2";
-
-            // 编码格式
-            public static string charset = "UTF-8";
         }
     }
 }
--- ACE_Behind_Mall.WebApi/Notify_url.aspx.cs
+++ ACE_Behind_Mall.WebApi/Notify_url.aspx.cs
@@ -6,11 +6,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ACE_Mall.BLL;
+using ACE_Mall.Model;
+using ACE_Mall.Common.App_Code;
 
 public partial class Notify_url : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        OrderBLL orderbll = new OrderBLL();
         /* 实际验证过程建议商户添加以下校验。
         1、商户需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号，
         2、判断total_amount是否确实为该订单的实际金额（即商户订单创建时的金额），
@@ -20,6 +24,7 @@ public partial class Notify_url : System.Web.UI.Page
         Dictionary<string, string> sArray = GetRequestPost();
         if (sArray.Count != 0)
         {
+
             bool flag = AlipaySignature.RSACheckV1(sArray, config.alipay_public_key, config.charset, config.sign_type, false);
             if (flag)
             {
@@ -32,7 +37,18 @@ public partial class Notify_url : System.Web.UI.Page
                 //注意：
                 //退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
                 string trade_status = Request.Form["trade_status"];
-
+                string out_trade_no = Request.Form["out_trade_no"];
+                decimal total_fee = Convert.ToDecimal(Request.Form["total_fee"]);
+                DateTime gmt_payment = Convert.ToDateTime(Request.Form["gmt_payment"]);
+                var ordermodel = orderbll.GetList(x => x.IsDelete == 0 && x.OrderNo == out_trade_no).FirstOrDefault();
+                if (trade_status == "TRADE_SUCCESS"||trade_status== "TRADE_FINISHED")
+                {
+                    ordermodel.OrderState = 2;
+                    ordermodel.PayMoney = total_fee;
+                    ordermodel.PayTime = gmt_payment;
+                    My_Order m = orderbll.GetUpdateModel<My_Order>(ordermodel, "ID");
+                    orderbll.Update(m);
+                }
                 Response.Write("success");
             }
             else
@@ -47,7 +63,6 @@ public partial class Notify_url : System.Web.UI.Page
         int i = 0;
         Dictionary<string, string> sArray = new Dictionary<string, string>();
         NameValueCollection coll;
-        //coll = Request.Form;
         coll = Request.Form;
         String[] requestItem = coll.AllKeys;
         for (i = 0; i < requestItem.Length; i++)
@@ -56,33 +71,5 @@ public partial class Notify_url : System.Web.UI.Page
         }
         return sArray;
 
-    }
-    public class config
-    {
-        public config()
-        {
-            //
-            // TODO: 在此处添加构造函数逻辑
-            //
-        }
-        // 应用ID,您的APPID
-        public static string app_id = "2016092700604341";
-
-        // 支付宝网关
-        public static string gatewayUrl = "https://openapi.alipaydev.com/gateway.do";
-
-        // 商户私钥，您的原始格式RSA私钥
-        public static string private_key = "MIIEpAIBAAKCAQEAtaWn9+3hgjIJLisH6W6fXk0PLuzFOffFdTZCfNFzwRMPu0ngXN7bNaXSOP55nPizsDvnjOTQPgGlEQj5q2G/6hfvHcSukLWR4Gp8wTgHzZAa8Us7w+sr49XK2PyursU8vwC3zc2JOLytH9mVGg8ky5b5uZYJpiZAM93NEFn0qKQObDrPuKi6S4roee7DrD4TglU20y2QbDJV9QUO/h2khz40N1ivkb/Ssz2/6gegCO1B+qbDqtfn/yMpqIpGLh3ex5TiRUi0o7bE65aI8cy403CXB088T47d/ivemj719+PMDoz4j4DyrZuytJAcKyalejFy/dTf7xfzE3nBrCubLwIDAQABAoIBAErjR/OcPAucK1LAZNulQsjzBh53ePxkHSEsxI6HH6zq+eJiG8DOFBvzCE99ApBnrFUs7VKWBxB5Rx9VzvEMNL4DLg7cxodchA68eECEpBronWL7fGFSdF7TBnUc/7uID3pDhoOviDI+/zURVDpRIf9ZeA0+QS4huPhpKiDipU5c2lN80VQb4Mx/CPzILHgVlUA9Haw7cVQdaMS4g9EDapvSe840awtqI0623tLzWlVVRFO1kf0OcYaqrZXnNPrUwz7c4wbqP88s//+Vlj4rt5yXq31iIpNOAtnpx8Hf5GLf3oY1ixSSentK2/tI9FCYMseDsfMYvrYls1jtSmazBckCgYEA6FmYe6Efrf0HBPIOA/u9+mgtMBCqohGFqPkXrocz6B0Es1XgyJGpjNR1t41YItFaGBKeBMF28Sq238Ga+TwN3c6JUgS++ltpqsjAassG3h82fIKNnOBdywFFfinWOrI4xIB8JL/dFqdXPTjB8XYHsEnasOq43G5vWHxEh+3ow7sCgYEAyCLhzVA7DWIewm/eAhiSPTjLX39kCbF3Xxv/LzMvUpamsdl6nodxRYyrw6Ipor/zG9/ZUVRYZySnXwjur5GLhragHsFTl1hCMechSuc+n1KJ+bnKWXfYmzoZ6dFUjqN08/XVUDbc5bgpzb1502ks6R7hvWCKXmBkmTOOJMGv3R0CgYEAln7Z7lHZpCd88W4bN+dKETSC+FwDOcBhs6XL+gamz1RkZAhe9WqryIUgzkgl6z9wvKDqaygoc2L4WowbZg3I6X9KoK5kSOMG2VD4mPNyOlM7RoCFWzRUbBYhves5UIF5lYWnY2JGwNKZBhWKQWzy4/OiS+9s15JMWqL6/JRTrNsCgYBRC2cbCFFRgxlnrXtep+qYZiZdHq45CqeHWUQGLhkMvbr1LFduWPSysFtiFon7wPVGpWhQefJumjY40totOKgivlOrAKjEEdaEdM9TkL8YL4l2GhlqD5Ekkuupdr5iIKkcncFrATyEvgYXrZHm3aF1Ka9KTAzcWPaD34/BLLn1tQKBgQCkr4B6z5vwbA2NVSL6uQp3fM2m6EJb5YFPIOnL6Zx/25g9pxdISDD2SK+f4UhCXPJchrr2Cq4lRnOQ7u0Bnw4ZFGdSb0hD/DTuhaTFxNgQQWbOcQbOyIkEQuZTSfMkcoFvpWEcqNAhEN6PSVklu9tAWfwEi/kM3EXc5nhq+4MiwQ==";
-
-
-        // 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
-        public static string alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu3wgGzFzl6gNeCdastRmI5nL/9pno2ENN13quRjpviAYzmG6CnIIIC0JhnlkwR3bU7Yj4FX6az3TD5P8UPp+MSxW8XC9vaPX2zUKxs35ZaBSuRePusdv3rqQ6HbtroLt5jVZ5fI1qqyn9HqoI/oFaOzi+DXmFntlKszMrwKBIbUE90ooHdAmQA2Pv1e1mJRi/4q0svU0mrNigQUgcu/PHsOt9PWmUKyl2EBGCLx2HHGLgKPY8ttfUYqzEFZGW7ZjP45982rwSIZH64WYul86ClVutrOmlZkHo6cDkugGURx5SEqzTvngvMRsPpzvk20OWOSZ7CQuUmeDtqmWz4Y0IQIDAQAB";
-
-
-        // 签名方式
-        public static string sign_type = "RSA2";
-
-        // 编码格式
-        public static string charset = "UTF-8";
     }
 }
