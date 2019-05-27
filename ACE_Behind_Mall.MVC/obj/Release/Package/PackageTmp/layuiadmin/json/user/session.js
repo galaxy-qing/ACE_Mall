a7f5c82372b0ae3af6f220734dd75db9c722fef5
galaxy-qing <44334049+galaxy-qing@users.noreply.github.com>
2019-05-27 16:30:22 +0800

    .

--- ACE_Behind_Mall.MVC/obj/Release/Package/PackageTmp/layuiadmin/json/user/session.js
+++
@@ -1,9 +0,0 @@
-{
-  "code": 0
-  ,"msg": ""
-  ,"data": {
-    "username": "galaxy-qing"
-    ,"sex": "男"
-    ,"role": 1
-  }
-}
--- ACE_Mall.Common/FileHelper.cs
+++ ACE_Mall.Common/FileHelper.cs
@@ -40,8 +40,8 @@ namespace ACE_Mall.Common
                 bmp.Save(Pfpath + picturename + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);   //保存为.jpg格式
                 ms.Close();
 
-                return "http://192.168.0.143:60391" + fpath + picturename + ".jpg";
-
+                // return "http://192.168.0.143:60391" + fpath + picturename + ".jpg";
+                return "http://47.101.45.222:2333" + fpath + picturename + ".jpg";
             }
             catch (Exception err)
             {
